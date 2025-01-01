397268fabfd8055d81c4919066036c694b95c718
bhavy1204 <bhavysarwa51@gmail.com>
2025-01-01 18:02:32 +0530

    Finished array basis in c

--- c/array.c
+++ c/array.c
@@ -1,6 +1,11 @@
 //If you have some understanding of array than you can skip it
+
+//IMAGINE :- if you have to store the roll numbers of 1000 student how will you do it...? if you want to store eggs how do you store them...? 
+
 //Defination :- Array are collection of homogenous(similar) elements.
-//Indexing in array starts from 0, i.e if the size of the array is 10 then it will store the elemnts till 9th index and last element(10) will be a null character '\0'.
+//Indexing in array starts from 0, i.e if the size of the array is 10 then it will store the elemnts till 9th index.
+
+// Last element is not a null character unless explicitly initialized or used in a string.
 
 #include <stdio.h>
 #include <Windows.h>
@@ -22,7 +27,8 @@ int main()
         printf("%d ", array[i]);
     }
 
-    //If we tried to access the last elemt 5, Than at place of that it will give garbage value. But it might be very harmful for the code so try to avoid accessing the elemnt beyound the scope of array.
+    //If we tried to access the last elemt 5,
+    // Last element is not a null character unless explicitly initialized or used in a string.
     printf("\nAccessing last element : -"); 
     for (int i = 0; i <= 5; i++)
     {
@@ -45,5 +51,29 @@ int main()
     }
     //We can also access the middle elemnts of the array directly;
     printf("%d", array[3]);
+
+    //Similarly we can also make arrays for float,chars.
+    char arr[5];
+    //Asks from user and assign it to array 
+    //TRY : give the string like "abcde" in the first iteration of the loop and observe what happens.
+    for (int i = 0; i < 5; i++)
+    {
+        printf("\nEnter char : ");
+        scanf("%c", &arr[i]);
+    }
+    //TRY : change the %c format specifier and observe the changes
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%c ", arr[i]);
+    }
+    //This was about dynamic array means that here we were known about the size of the array like 5 or any contant number.
+
+    // we can not do the following in c because the array size must be known at compile time only, It can not be a variable.
+    // int n;
+    // printf("Enter n : ");
+    // scanf("%d", &n);
+    // int arr[n];
+
+    //QUESTION : List any one advantage and disadvantage of array. The answer will be given in next code(dynamicArray.c).
     return 0;
 }
