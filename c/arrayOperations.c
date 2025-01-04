89f9412f097dce6ebe00ae0a6d5d1f3fb8694db7
bhavy1204 <bhavysarwa51@gmail.com>
2025-01-04 09:54:27 +0530

    Added array operations

---
+++ c/arrayOperations.c
@@ -0,0 +1,31 @@
+//In the following code we will do general operations peformed on an array
+
+//General operation that can be peformed are :- 
+
+// 1. Searching : It refers to the act of findig wether a particular value is present in the group of elements. Searching is of further 2 types binary and linear search. In this file we will only see linear search. In this file we will also look for the searching of elemts before and after the given the given value.
+
+// 2. Sorting : It is the act of arranging the given elements in some order, Generally ascending or descending order. There are many efficient algorithms for searching but here we will only use bubble sort for sake of simplicity.
+
+// 3. Traversal : It is act of visiting each element atleast once and peforming the desired operation on it, Generally used for display.
+
+// 4. Addition and deletion of elemts : It is most common operation. They can in peformed in begineing,end or in middle.
+
+// 5. Accessing elemts and their index : It is act of getting and element from the array based on its index, It can be similar to searching in some way.
+
+//THINK : Where can we use all this operations in a real life all at one place..? The answer is given at last but think 2-3 examples before it.
+
+//We will create functions for each operation peformed and In this example we are taking an int array but you should try it with float and character array as well.
+
+
+
+#include <stdio.h>
+int main()
+{
+    int choice;
+    do
+    {
+        /* code */
+    } while (choice!=5);
+    
+    return 0;
+}
