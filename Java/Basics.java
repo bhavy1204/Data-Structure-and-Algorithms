72a705ec6053e6921cef58caa7523c52620501b3
bhavy1204 <bhavysarwa51@gmail.com>
2025-01-07 09:49:19 +0530

    Basic examples of java

--- Java/Basics.java
+++ Java/Basics.java
@@ -1,6 +1,32 @@
-package Java;
-public class Basics {
-    public static void main(String[] args) {
-        System.out.println("Helllo world");
-    }
-}
+// package Java;
+
+// public class Basics {
+//     public static void main(String[] args) {
+//         System.out.print("\033[H\033[2J");
+//         System.out.flush();
+//         System.out.println("Simple java program");
+//     }
+// }
+
+// package Java;
+// public class Basics{
+//     public static void main(String[] args){
+//         System.out.println("\033[H\033[2J");
+//         System.out.flush();
+//         String s="Hello I am in a string";
+//         System.out.print(s);
+//     }
+// }
+
+// package Java;
+
+// public class Basics {
+//     public static void main(String[] args) {
+//         System.out.println("\033[H\033[2J");
+//         System.out.flush();
+//         int arr[] = { 10, 20, 30 };
+//         for (int i = 0; i < 3; i++) {
+//             System.out.println(arr[i]);
+//         }
+//     }
+// }
